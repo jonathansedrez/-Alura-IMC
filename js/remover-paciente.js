58c012abfa00ac48d3aa1c1543d54d8f45a592c0
jonathan <jonathansedrez@gmail.com>
2018-06-12 14:09:09 -0300

    atualizado

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -4,8 +4,7 @@ tabela.addEventListener("dblclick", function(event) {
      if (event.target.tagName == 'TD') {
 	    event.target.parentNode.classList.add("fadeOut"); //Da ao item clicado a classe fadeOut que desaparece o item
 	    setTimeout(function() { //Só vai executar o codigo dpeois de um tempo, 0,5 segundos (sleep)
-	    event.target.parentNode.remove(); 
-	    //event: pega o evento, target: região que foi clicada, parentNode: Pai de quem foi clicado (no caso a tr inteira)
+	    event.target.parentNode.remove(); //event: pega o evento, target: região que foi clicada, parentNode: Pai de quem foi clicado (no caso a tr inteira)
 	    }, 500);
 	}
 });
